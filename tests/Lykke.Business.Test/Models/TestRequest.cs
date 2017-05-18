5db1c32a65e9dae0a9c18de77c76a4ae1426a4a0
Anton Sivov <anton.sivov@inbox.ru>
2017-05-18 11:04:07 +0300

    New file structure apply.

---
+++ tests/Lykke.Business.Test/Models/TestRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LykkePay.Business;
+
+namespace LykkePay.Business.Test.Models
+{
+    class TestRequest:BaseRequest
+    {
+        public string Test1 { get; set; }
+        public int Test2 { get; set; }
+        public string Test3 { get; set; }
+        public int Test4 { get; set; }
+    }
+}
