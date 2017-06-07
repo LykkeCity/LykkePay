7553299ef4b38dd3503d1af9343e4ab6cf033e10
Anton Sivov <anton.sivov@inbox.ru>
2017-06-07 16:30:03 +0300

    Created new micro service to generate public/private keys for merchant with assertId and store it.

---
+++ src/Lykke.Pay.Service.GenerateAddress/Controllers/GenerateController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Lykke.AzureRepositories;
+using Microsoft.AspNetCore.Mvc;
+using Lykke.Common.Entities.Pay;
+using Lykke.Common.Entities.Security;
+using Lykke.Core;
+using Lykke.Pay.Service.GenerateAddress.Code;
+using Newtonsoft.Json;
+
+namespace Lykke.Pay.Service.GenerateAddress.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/generate")]
+    public class GenerateController : Controller
+    {
+        private readonly PayServiceGenAddressSettings _settings;
+        private readonly IMerchantWalletRepository _merchantWalletRepository;
+
+        public GenerateController(PayServiceGenAddressSettings settings, IMerchantWalletRepository merchantWalletRepository)
+        {
+            _settings = settings;
+            _merchantWalletRepository = merchantWalletRepository;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<string> Post([FromBody]GenerateAddressRequest request)
+        {
+            if (string.IsNullOrEmpty(request?.MerchantId) || string.IsNullOrEmpty(request.AssertId))
+            {
+                return string.Empty;
+            }
+
+            using (var rsa = RSA.Create())
+            {
+                rsa.KeySize = _settings.GenerateKeySize;
+                var pbl = rsa.ExportParameters(true);
+
+                var dateToStore = new AssertPrivKeyPair
+                {
+                    AssertId = request.AssertId,
+                    PrivateKey = pbl.GetPrivateKey()
+                };
+
+                var encriptedData = SavePrimaryKey(JsonConvert.SerializeObject(dateToStore));
+                await _merchantWalletRepository.SaveNewAddress(new MerchantWalletEntity
+                {
+                    MerchantId = request.MerchantId,
+                    Data = encriptedData
+                });
+                return pbl.GetPublicKey();
+            }
+
+        }
+
+        private string SavePrimaryKey(string getPrivateKey)
+        {
+
+            byte[] result;
+            using (var aes = Aes.Create())
+            using (var md5 = MD5.Create())
+            using (var sha256 = SHA256.Create())
+            {
+                aes.Key = sha256.ComputeHash(Encoding.UTF8.GetBytes(_settings.DataEncriptionPassword));
+                aes.IV = md5.ComputeHash(Encoding.UTF8.GetBytes(_settings.DataEncriptionPassword));
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                using (var resultStream = new MemoryStream())
+                {
+                    using (var aesStream = new CryptoStream(resultStream, encryptor, CryptoStreamMode.Write))
+                    using (var plainStream = new MemoryStream(Encoding.UTF8.GetBytes(getPrivateKey)))
+                    {
+                        plainStream.CopyTo(aesStream);
+                    }
+
+                    result = resultStream.ToArray();
+                }
+            }
+
+            return Convert.ToBase64String(result);
+        }
+    }
+}
---
+++ src/Lykke.Pay.Service.GenerateAddress/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Lykke.AzureRepositories;
+using Lykke.Pay.Service.GenerateAddress.Code;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Lykke.SettingsReader;
+
+namespace Lykke.Pay.Service.GenerateAddress
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            BuildConfiguration(services);
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        private void BuildConfiguration(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetValue<string>("ConnectionString");
+           
+#if DEBUG
+            var settings = SettingsReader.SettingsReader.ReadGeneralSettings<Settings>(connectionString);
+#else
+            var settings = SettingsReader.SettingsReader.ReadGeneralSettings<Settings>(new Uri(connectionString));
+#endif
+
+
+          
+
+            services.AddSingleton(settings.PayServiceGenAddress);
+            services.RegisterRepositories(settings.PayServiceGenAddress.Db.PrivateKeysConnString, null);
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
