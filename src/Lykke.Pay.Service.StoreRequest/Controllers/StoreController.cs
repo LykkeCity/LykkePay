920dc515d55ac1239e2238f2656980ec233c672a
Anton Sivov <anton.sivov@inbox.ru>
2017-06-20 21:12:43 +0300

    Added new store request project

---
+++ src/Lykke.Pay.Service.StoreRequest/Controllers/StoreController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.Pay.Service.StoreRequest.Controllers
+{
+    [Route("api/Store")]
+    public class StoreController : Controller
+    {
+      
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+       
+    }
+}
