10e27d91af79cf2cb783b6954594a38a7319706c
Anton Sivov <anton.sivov@inbox.ru>
2017-06-21 20:41:01 +0300

    Few bugs

--- src/LykkePayAPI/Controllers/TransferController.cs
+++ src/LykkePayAPI/Controllers/TransferController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using LykkePay.API.Code;
 using LykkePay.API.Models;
