2eaa6c4cc689086d9be197aebd7c692044f79b78
Anton Sivov <anton.sivov@inbox.ru>
2017-06-28 01:24:52 +0300

    Added controller to get list of wallet addresses.

---
+++ src/Lykke.Pay.Service.GenerateAddress/Code/HttpContextExtention.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Lykke.Pay.Service.GenerateAddress.Code
+{
+    public static class HttpContextExtention
+    {
+        public static string GetRealIp(this HttpContext context)
+        {
+            string ip = string.Empty;
+
+            // http://stackoverflow.com/a/43554000/538763
+            var xForwardedForVal = GetHeaderValueAs<string>(context, "X-Forwarded-For").SplitCsv().FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(xForwardedForVal))
+            {
+                ip = xForwardedForVal.Split(':')[0];
+            }
+
+            // RemoteIpAddress is always null in DNX RC1 Update1 (bug).
+            if (string.IsNullOrWhiteSpace(ip) && context.Connection?.RemoteIpAddress != null)
+                ip = context.Connection.RemoteIpAddress.ToString();
+
+            if (string.IsNullOrWhiteSpace(ip))
+                ip = GetHeaderValueAs<string>(context, "REMOTE_ADDR");
+
+            return ip;
+        }
+
+        private static T GetHeaderValueAs<T>(HttpContext httpContext, string headerName)
+        {
+            StringValues values;
+
+            if (httpContext?.Request?.Headers?.TryGetValue(headerName, out values) ?? false)
+            {
+                string rawValues = values.ToString();   // writes out as Csv when there are multiple.
+
+                if (!string.IsNullOrEmpty(rawValues))
+                    return (T)Convert.ChangeType(values.ToString(), typeof(T));
+            }
+            return default(T);
+        }
+
+        private static List<string> SplitCsv(this string csvList, bool nullOrWhitespaceInputReturnsNull = false)
+        {
+            if (string.IsNullOrWhiteSpace(csvList))
+                return nullOrWhitespaceInputReturnsNull ? null : new List<string>();
+
+            return csvList.TrimEnd(',').Split(',').Select(s => s.Trim()).ToList();
+        }
+    }
+}
