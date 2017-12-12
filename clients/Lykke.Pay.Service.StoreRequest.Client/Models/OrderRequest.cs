6f94032de2c4195075bf66d137835f57ce03b0cb
Anton Sivov <anton.sivov@inbox.ru>
2017-12-13 01:54:17 +0300

    Updated Client

--- clients/Lykke.Pay.Service.StoreRequest.Client/Models/OrderRequest.cs
+++ clients/Lykke.Pay.Service.StoreRequest.Client/Models/OrderRequest.cs
@@ -26,7 +26,7 @@ namespace Lykke.Pay.Service.StoreRequest.Client.Models
         /// 'New', 'InProgress', 'Failed', 'Completed'</param>
         /// <param name="merchantPayRequestNotification">Possible values
         /// include: 'Nothing', 'InProgress', 'Success', 'Error'</param>
-        public OrderRequest(string merchantId = default(string), string requestId = default(string), string transactionId = default(string), PayFee markup = default(PayFee), string merchantPayRequestStatus = default(string), string merchantPayRequestNotification = default(string), string sourceAddress = default(string), string assetPair = default(string), string exchangeAssetId = default(string), double? amount = default(double?), double? exchangeRate = default(double?), string assetId = default(string), string successUrl = default(string), string errorUrl = default(string), string progressUrl = default(string), string orderId = default(string), string transactionDetectionTime = default(string), string transactionWaitingTime = default(string))
+        public OrderRequest(string merchantId = default(string), string requestId = default(string), string transactionId = default(string), PayFee markup = default(PayFee), string merchantPayRequestStatus = default(string), string merchantPayRequestNotification = default(string), string sourceAddress = default(string), string assetPair = default(string), string exchangeAssetId = default(string), double? amount = default(double?), double? originAmount = default(double?), double? exchangeRate = default(double?), string assetId = default(string), string successUrl = default(string), string errorUrl = default(string), string progressUrl = default(string), string orderId = default(string), string transactionDetectionTime = default(string), string transactionWaitingTime = default(string))
         {
             MerchantId = merchantId;
             RequestId = requestId;
@@ -38,6 +38,7 @@ namespace Lykke.Pay.Service.StoreRequest.Client.Models
             AssetPair = assetPair;
             ExchangeAssetId = exchangeAssetId;
             Amount = amount;
+            OriginAmount = originAmount;
             ExchangeRate = exchangeRate;
             AssetId = assetId;
             SuccessUrl = successUrl;
@@ -107,6 +108,11 @@ namespace Lykke.Pay.Service.StoreRequest.Client.Models
         /// </summary>
         [JsonProperty(PropertyName = "amount")]
         public double? Amount { get; set; }
+
+        /// <summary>
+        /// </summary>
+        [JsonProperty(PropertyName = "originAmount")]
+        public double? OriginAmount { get; set; }
 
         /// <summary>
         /// </summary>
