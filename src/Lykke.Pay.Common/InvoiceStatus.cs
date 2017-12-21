c3fdd163c1d76b8c1d12bdb35e0d88c82065689c
Anton Sivov <anton.sivov@inbox.ru>
2017-12-21 14:11:50 +0300

    Added Invoice Status to Common

---
+++ src/Lykke.Pay.Common/InvoiceStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Pay.Common
+{
+    public enum InvoiceStatus
+    {
+        Draft,
+        Paid,
+        Unpaid,
+        Removed,
+        Underpaid,
+        Overpaid,
+        LatePaid
+
+    }
+}
