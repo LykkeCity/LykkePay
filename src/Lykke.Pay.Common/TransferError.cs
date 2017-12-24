a43d6cf6b509721615f9b18e1ee71ab1f6e89c91
Anton Sivov <anton.sivov@inbox.ru>
2017-12-24 12:02:07 +0300

    Added correct statuses for Payment

--- src/Lykke.Pay.Common/TransferError.cs
+++ src/Lykke.Pay.Common/TransferError.cs
@@ -7,14 +7,4 @@
         INVALID_ADDRESS,
         INTERNAL_ERROR
     }
-
-    public enum PaymentError
-    {
-        TRANSACTION_NOT_CONFIRMED,
-        TRANSACTION_NOT_DETECTED,
-        AMOUNT_BELOW,
-        AMOUNT_ABOVE,
-        PAYMENT_EXPIRED
-
-    }
 }
