5d3073ef273a93f0bfd7664abe2960b00ee52e41
Alexey Novichikhin <alexey.novichikhin@lykke.com>
2018-01-13 17:44:31 +0700

    use request log middleware

---
+++ src/LykkePayAPI/Middleware/RequestLogMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Log;
+using Microsoft.AspNetCore.Http;
+
+namespace LykkePay.API.Middleware
+{
+    public class RequestLogMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILog _log;
+
+        public RequestLogMiddleware(RequestDelegate next, ILog log)
+        {
+            _next = next;
+            _log = log;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = context.Request;
+            var merchantId = request.Headers["Lykke-Merchant-Id"].ToString();
+            try
+            {
+                await _log.WriteInfoAsync("LykkePayApi", $"{request.Method} {request.Path}", $"merchantId: {merchantId}", "receive request");
+
+                await _next(context);
+
+                await _log.WriteInfoAsync("LykkePayApi", $"{request.Method} {request.Path}", $"merchantId: {merchantId}", $"responce status code: {context.Response?.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                await _log.WriteWarningAsync("LykkePayApi", $"{request.Method} {request.Path}", $"merchantId: {merchantId}", $"Exception on execute request", ex);
+                throw;
+            }
+            
+        }
+    }
+}
